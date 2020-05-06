a9d80c1657c9aa2f577c760c867fe505a646570e
Jack Heriz-Smith <jackherizsmith@gmail.com>
2020-05-06 15:51:14 +0100

    invented a snake game from scratch

---
+++ src/food.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default ({ dot }) => {
+
+    const style = {
+        left: `${dot[0]}%`,
+        top: `${dot[1]}%`,
+    }
+
+    return (
+        <div className="snake-food" style={style}></div>
+    )
+}
